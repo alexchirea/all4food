edc850f7a264b50c3e0a8843a77f9f283a1a4bb6
Alex Chirea <ac.chirea@gmail.com>
2020-03-08 12:28:46 +0200

    first commit

---
+++ src/main/java/com/echipa11/all4food/All4foodApplication.java
@@ -0,0 +1,13 @@
+package com.echipa11.all4food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class All4foodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(All4foodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echipa11/all4food/All4foodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echipa11.all4food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class All4foodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
