39174ab080cd1d4b7746108c7a223dbfbeac9ac9
Alexandru Chirea <alexandru.chirea@sap.com>
2020-03-10 13:33:02 +0200

    Spring Security Implementation

---
+++ src/main/java/com/echipa11/all4food/controller/ComenziController.java
@@ -0,0 +1,12 @@
+package com.echipa11.all4food.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/comenzi")
+public class ComenziController {
+
+    /** Controller pentru clienti */
+
+}
