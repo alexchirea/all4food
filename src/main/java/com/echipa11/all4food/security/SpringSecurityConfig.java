e204206ea1f7bdad9d3688a6c18f9db3dd381eba
Alex Chirea <ac.chirea@gmail.com>
2020-05-03 13:45:58 +0300

    disable security for testing purposes

--- src/main/java/com/echipa11/all4food/security/SpringSecurityConfig.java
+++ src/main/java/com/echipa11/all4food/security/SpringSecurityConfig.java
@@ -1,72 +1,72 @@
-package com.echipa11.all4food.security;
-
-import com.echipa11.all4food.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandler;
-
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private AccessDeniedHandler accessDeniedHandler;
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService();
-    };
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    };
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
-    }
-
-    // roles admin allow to access /admin/**
-    // roles user allow to access /user/**
-    // custom 403 access denied handler
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        /** Disable security for testing purposes */
-        http.authorizeRequests().antMatchers("/").permitAll();
-
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/", "/home", "/about", "/contact").permitAll()
-//                .antMatchers("/admin/**").hasAnyRole("ADMIN")
-//                .antMatchers("/**").hasAnyRole("USER", "ADMIN")
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll()
-//                .and()
-//                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
-    }
-
-    // create two users, admin and user
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//package com.echipa11.all4food.security;
 //
-//        auth.inMemoryAuthentication()
-//                .withUser("client").password("{noop}all4food").roles("USER")
-//                .and()
-//                .withUser("admin").password("{noop}all4food").roles("ADMIN");
+//import com.echipa11.all4food.service.CustomUserDetailsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.access.AccessDeniedHandler;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    private AccessDeniedHandler accessDeniedHandler;
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new CustomUserDetailsService();
+//    };
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    };
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
 //    }
-}
+//
+//    // roles admin allow to access /admin/**
+//    // roles user allow to access /user/**
+//    // custom 403 access denied handler
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        /** Disable security for testing purposes */
+//        http.authorizeRequests().antMatchers("/").permitAll();
+//
+////        http.csrf().disable()
+////                .authorizeRequests()
+////                .antMatchers("/", "/home", "/about", "/contact").permitAll()
+////                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+////                .antMatchers("/**").hasAnyRole("USER", "ADMIN")
+////                .anyRequest().authenticated()
+////                .and()
+////                .formLogin()
+////                .loginPage("/login")
+////                .permitAll()
+////                .and()
+////                .logout()
+////                .permitAll()
+////                .and()
+////                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+//    }
+//
+//    // create two users, admin and user
+////    @Autowired
+////    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+////
+////        auth.inMemoryAuthentication()
+////                .withUser("client").password("{noop}all4food").roles("USER")
+////                .and()
+////                .withUser("admin").password("{noop}all4food").roles("ADMIN");
+////    }
+//}
--- src/main/java/com/echipa11/all4food/service/CustomUserDetailsService.java
+++ src/main/java/com/echipa11/all4food/service/CustomUserDetailsService.java
@@ -1,44 +1,44 @@
-package com.echipa11.all4food.service;
-
-import com.echipa11.all4food.model.User;
-import com.echipa11.all4food.repository.UserRepository;
-import com.echipa11.all4food.security.CustomUserPrincipal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        User user = findUserbyUername(username);
-
-        UserBuilder builder = null;
-        if (user != null) {
-            builder = org.springframework.security.core.userdetails.User.withUsername(username);
-            builder.password(new BCryptPasswordEncoder().encode(user.getPassword()));
-            builder.roles(user.getRole());
-        } else {
-            throw new UsernameNotFoundException("User not found.");
-        }
-
-        return builder.build();
-    }
-
-    private User findUserbyUername(String username) {
-        if(username.equalsIgnoreCase("admin")) {
-            return new User(username, "all4food", "ADMIN");
-        }
-        return null;
-    }
-
-}
+//package com.echipa11.all4food.service;
+//
+//import com.echipa11.all4food.model.User;
+//import com.echipa11.all4food.repository.UserRepository;
+//import com.echipa11.all4food.security.CustomUserPrincipal;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.core.userdetails.User.UserBuilder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        User user = findUserbyUername(username);
+//
+//        UserBuilder builder = null;
+//        if (user != null) {
+//            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+//            builder.password(new BCryptPasswordEncoder().encode(user.getPassword()));
+//            builder.roles(user.getRole());
+//        } else {
+//            throw new UsernameNotFoundException("User not found.");
+//        }
+//
+//        return builder.build();
+//    }
+//
+//    private User findUserbyUername(String username) {
+//        if(username.equalsIgnoreCase("admin")) {
+//            return new User(username, "all4food", "ADMIN");
+//        }
+//        return null;
+//    }
+//
+//}
